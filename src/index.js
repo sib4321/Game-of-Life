f50dbb5e7b80e2265d60682ca1b4500dac105757
sib4321 <sib4321@ukr.net>
2017-11-22 14:51:06 +0200

    Create grid

--- src/index.js
+++ src/index.js
@@ -2,11 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Grid extends React.Component {
+  render() {
+    const width = this.props.cols * 14;
+    var rowsArr = []
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        )
+      }
+    }
+
+    return (
+      <div className="grid" style={{width: width}}>
+        {{rowsArr}}
+
+      </div>
+    );
+  }
+}
+
 class Main extends React.Component {
   constructor() {
     super();
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50;
+
     this.state = {
       generation: 0,
+      gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
   }
 
@@ -15,6 +53,10 @@ class Main extends React.Component {
       <div>
         <h1>The Game of Life</h1>
         <Grid
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selectBox={this.selectBox}
         />
         <h2>Generations: {this.state.generation}</h2>
       </div>
